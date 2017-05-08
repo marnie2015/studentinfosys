160dd971183909985a763e4fafb15f1cd3e5ab0c
Marnie Paraiso <marnie.paraiso@paramount.com.ph>
2017-05-08 14:04:12 +0800

    add user accounts maintenance

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   resources :schedules
   resources :subjects
   get '/grades' => 'grades#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507064610) do
+ActiveRecord::Schema.define(version: 20170508053919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 20170507064610) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "user_pass"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "year_levels", force: :cascade do |t|
