ec17f30aa1ec8e4c6abf9b91f22722ee0f22873b
Marnie Paraiso <marnie.paraiso@paramount.com.ph>
2017-06-24 10:27:31 +0800

    don't show the students account in user page maintenance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where("access < 3")
   end
 
   # GET /users/1
