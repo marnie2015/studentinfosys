17840380fdda9f13f1f5474c8dc62a6b5df2de5f
Marnie Paraiso <marnie.paraiso@paramount.com.ph>
2017-11-27 07:33:09 +0800

    for update on teachers page

--- db/migrate/20170510080114_create_teachers.rb
+++ db/migrate/20170510080114_create_teachers.rb
@@ -4,6 +4,8 @@ class CreateTeachers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.references :position, index: true, foreign_key: true
+      t.references :subject
+      t.references :room
       t.integer :user_id
 
       t.timestamps null: false
